f13377911bcaf828408e02083b3d26cbc3cca1d2
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-10-22 17:24:26 +0200

    ee

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+
+?>
+
+
+<script language="JavaScript" src="http://www.iplocationtools.com/iplocationtools.js?key=6a657e7c6c65716a7a7c627e663962667d"></script>
+
+<fieldset>
+<legend>Ejemplo de geolocalizacion</legend>
+Hola, creo que estas en:
+
+<script language="JavaScript">
+document.write(ip2location_city() + ', ' + ip2location_region() + ', ' + ip2location_country_long() + ', ' + ip2location_zip_code());
+</script>
+
+</fieldset>
+	
